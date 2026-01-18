5667a213a25f9fc9d47ac17ec53946869d372a33
Dmitry Omelchenko <omd1337@gmail.com>
2026-01-18 01:44:17 +0000

    2026.01.17-a4cc0e7dd

--- src/com/hypixel/hytale/server/spawning/controllers/BeaconSpawnController.java
+++ src/com/hypixel/hytale/server/spawning/controllers/BeaconSpawnController.java
@@ -72,6 +72,10 @@ public class BeaconSpawnController extends SpawnController<NPCBeaconSpawnJob> {
       assert legacySpawnBeaconComponent != null;
       BeaconSpawnWrapper wrapper = legacySpawnBeaconComponent.getSpawnWrapper();
       RoleSpawnParameters spawn = wrapper.pickRole(ThreadLocalRandom.current());
+      if (spawn == null) {
+         return null;
+      }
+
       String spawnId = spawn.getId();
       int roleIndex = NPCPlugin.get().getIndex(spawnId);
       if (roleIndex >= 0 && !this.unspawnableRoles.contains(roleIndex)) {
